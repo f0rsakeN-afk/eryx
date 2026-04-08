0f62c11cd5bfc3502944e8420735cf92e194be4e
f0rsakeN-afk <nar3sh0@gmail.com>
2026-04-08 11:35:00 +0545

    remove backend

--- frontend/services/axios.ts
+++
@@ -1,9 +0,0 @@
-import { getBackendUrl } from "@/utils/config";
-import axios from "axios";
-
-const backendUrl = getBackendUrl();
-
-export const instance = axios.create({
-  baseURL: backendUrl,
-  withCredentials: true,
-});
